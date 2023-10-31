fdd8f5ab68683bb833d0b23f5961f22859381173
Aastha Agarwal <astha1916@gmail.com>
2023-10-31 13:55:57 +0530

    Project description and title added

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaGithub} from 'react-icons/fa';
 // import About_Me_old from '../assets/portfolio/About_Me_old.jpg';
 // import homepageimage from '../assets/portfolio/homepageimage.svg';
 // import one from '../assets/portfolio/one.svg';
@@ -14,17 +15,26 @@ export const Projects = () => {
     const projects = [
         {
             id: 1,
-            src: CollegeVerification
+            src: CollegeVerification,
+            title: "College Verification Portal",
+            description: "Our project serves as a platform empowering users to verify a college's accreditation status with the University Grants Commission (UGC).",
+            href: "https://github.com/Aastha1916/SIH-2022"
 
         },
         {
             id: 2,
-            src: WebScrapper
+            src: WebScrapper,
+            title: "Web Scrapper Bot",
+            description: "Developed during my third year of graduation, this project is a fundamental web scraping program.It extracts data from Amazon's website and saves it in a CSV file.",
+            href:"https://github.com/Aastha1916/WebScrapperBot"
 
         },
         {
             id: 3,
-            src: FaceMaskDetector
+            src: FaceMaskDetector,
+            title:"Face Mask Detector",
+            description:"Created during my first year of BTech, this Python project delves into Computer Vision. It centers on face detection for mask presence, a vital need stemming from the COVID-19 pandemic.",
+            href:"https://github.com/Aastha1916/Face_Detection_OpenCv"
 
         }
     ]
@@ -33,16 +43,22 @@ export const Projects = () => {
             <div className='max-w-screen-lg p-10 mx-auto flex flex-col justify-center h-full'>
                 <div >
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Projects</p>
-                    <p className='py-6'>Check out some of my work right here</p>
+                    <p className='py-6'>Check out some of my work right here :</p>
                 </div>
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-10 sm:px-0'>
                 {
-                    projects.map(({ id, src }) => (
+                    projects.map(({ id, src,href ,title,description}) => (
                             <div key={id}  className='shadow-md shadow-gray-600 rounded-lg'>
-                                <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'></img>
-                                <div className='flex items-center justify-center'>
-                                    <button className='w-1/2 px-6 py-3 m-4 duartion-200 hover:scale-105'>Demo</button>
-                                    <button className='w-1/2 px-6 py-3 m-4 duartion-200 hover:scale-105'>Code</button>
+                                <img src={src} alt="" className='rounded-md  hover:scale-105 hover:duration-300'></img>
+                                <div className='flex flex-col items-center justify-center px-4'>
+                                    <div className='sm:h-60 md:h-64'>
+                                    <h1 className=' py-4 font-bold text-xl md:h-20'>{title}</h1>
+                                    <p className='text-justify'>{description}</p>
+                                    </div>
+                                <a href={href} className='flex px-32 py-3 m-4 hover:scale-105  justify-around items-center w-full text-white text-xl lg:px-16 md:px-8 sm:px-14 hover:bg-slate-500 hover:rounded-xl hover:duration-500 ' target='_blank' rel="noreferrer">
+                                Code <FaGithub size={30}></FaGithub>
+                                </a> 
+                                    {/* <button className='w-1/2 px-6 py-3 m-4 duartion-200 hover:scale-105'>Code</button> */}
                                 </div>
                             </div>
                         
