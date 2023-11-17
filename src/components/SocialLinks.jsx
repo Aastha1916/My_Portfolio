2ea411d877893de69e07ce6ba86360e102d1d1cd
Aastha Agarwal <astha1916@gmail.com>
2023-11-17 15:10:13 +0530

    Github and leetcode accounts added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {FaGithub, FaLinkedin} from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import {BsFillPersonLinesFill} from "react-icons/bs";
+// import {BsFillPersonLinesFill} from "react-icons/bs";
+import GfG_logo from '../assets/logos/gfg_logo.png';
+
 const SocialLinks = () => {
     const links=[
         {
@@ -35,23 +37,45 @@ const SocialLinks = () => {
             
         },
         {
-            id: 4,
+        id: 4,
+        child:(
+            <>
+            <p>Leetcode</p>
+            <img width="30" height="30" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/96/FFFFFF/external-level-up-your-coding-skills-and-quickly-land-a-job-logo-bold-tal-revivo.png" alt="external-level-up-your-coding-skills-and-quickly-land-a-job-logo-bold-tal-revivo"/>
+            </>
+        ),
+        href:'https://leetcode.com/aastha1916/',
+        },
+        // {
+        //     id: 5,
+        //     child:(
+        //         <>
+        //         Resume <BsFillPersonLinesFill size={30}></BsFillPersonLinesFill>
+        //         </>
+        //     ),
+        //     href:'/Aastha_agarwal_resume.pdf',
+        //     style: 'rounded-br-md',
+        //     download:true,
+        // }
+        {
+            id: 5,
             child:(
                 <>
-                Resume <BsFillPersonLinesFill size={30}></BsFillPersonLinesFill>
+                <p>GeeksforGeeks</p>
+                <img height="40" width="40" src={GfG_logo} alt="GeeksForGeeks Logo"  className='-mr-1'></img>
                 </>
             ),
-            href:'/Aastha_agarwal_resume.pdf',
             style: 'rounded-br-md',
-            download:true,
+            href:'https://auth.geeksforgeeks.org/user/astha1916',
         }
+
     ]
     
   return (
-    <div className=' hidden lg:flex flex-col top-[35%] left-0 fixed'>
+    <div className=' hidden lg:flex flex-col top-[35%] left-0 fixed '>
         <ul>
             {links.map(({id,child,href,style,download}) => (
-                <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${style}`}>
+                <li key={id} className={`flex justify-between items-center w-48 h-14 px-4 ml-[-130px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${style}`}>
             <a href={href} className='flex justify-between items-center w-full text-white' download={download} target='_blank' rel="noreferrer">
             {child}
             </a> 
